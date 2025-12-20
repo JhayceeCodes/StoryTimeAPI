d25c643cda106f22b51c5b159dc0013576a8d916
JhayceeCodes <139480167+JhayceeCodes@users.noreply.github.com>
2025-12-20 04:39:48 +0100

    feat: add password reset and confirm endpoints

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import RegisterView, LoginView, SendVerificationEmailView, VerifyEmailView
+from .views import RegisterView, LoginView, SendVerificationEmailView, VerifyEmailView, RequestPasswordResetView, PasswordResetConfirmView
 
 urlpatterns = [ 
     path("register/", RegisterView.as_view(), name="register"),
     path("login/", LoginView.as_view(), name="login"),
     path("resend-email/", SendVerificationEmailView.as_view(), name="resend-email"),
-    path('verify-email/', VerifyEmailView.as_view(), name="verify-email")
+    path('verify-email/', VerifyEmailView.as_view(), name="verify-email"),
+    path("password-reset/", RequestPasswordResetView.as_view(), name="request password reset"),
+    path('reset/<uidb64>/<token>/', PasswordResetConfirmView.as_view(), name='confirm password reset'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -2,9 +2,12 @@ from rest_framework import generics, permissions, status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework_simplejwt.views import TokenObtainPairView
+from django.contrib.auth.password_validation import validate_password
+from django.core.exceptions import ValidationError
 from .serializers import RegisterSerializer, LoginSerializer
 from .tasks import send_password_reset_email_task, send_verification_email_task
 from .utils import generate_token, verify_token
+from .models import  User
 
 
 
@@ -69,5 +72,61 @@ class VerifyEmailView(APIView):
 
         return Response(
             {'message': 'Email verified successfully.'},
+            status=status.HTTP_200_OK
+        )
+
+    
+class RequestPasswordResetView(APIView):
+    def post(self, request):
+        email = request.data.get('email')
+        if not email:
+            return Response({'detail': 'Email is required'}, status=status.HTTP_400_BAD_REQUEST)
+
+        try:
+            user = User.objects.get(email=email)
+        except User.DoesNotExist:
+            return Response({'message': 'Password reset email sent'}, status=status.HTTP_200_OK)
+
+        uid, token = generate_token(user)
+        
+        reset_link = f"/reset-password/{uid}/{token}/"
+        
+        send_password_reset_email_task.delay(user.id, reset_link)
+
+        return Response({'message': 'Password reset email sent'}, status=status.HTTP_200_OK)
+
+    
+class PasswordResetConfirmView(APIView):
+    permission_classes = []
+
+    def post(self, request, uidb64, token):
+        user = verify_token(uidb64, token)
+
+        if not user:
+            return Response(
+                {"detail": "Invalid or expired token"},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        new_password = request.data.get("password")
+        if not new_password:
+            return Response(
+                {"detail": "Password is required"},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        try:
+            validate_password(new_password, user)
+        except ValidationError as e:
+            return Response(
+                {"detail": e.messages},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        user.set_password(new_password)
+        user.save(update_fields=["password"])
+
+        return Response(
+            {"message": "Password reset successful"},
             status=status.HTTP_200_OK
         )
