5e568e304ac5f6174a6a5f2e58ec6701ac6ec6c1
JhayceeCodes <139480167+JhayceeCodes@users.noreply.github.com>
2025-12-20 04:08:17 +0100

    fix: make email unique in user field

--- accounts/models.py
+++ accounts/models.py
@@ -26,6 +26,7 @@ class CustomUserManager(UserManager):
 
 
 class User(AbstractUser):
+    email = models.EmailField(unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
     is_verified = models.BooleanField(default=False)
 
