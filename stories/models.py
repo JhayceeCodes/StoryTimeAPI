e8833eba2dfedc24e7c05f85a59d70785d084914
JhayceeCodes <139480167+JhayceeCodes@users.noreply.github.com>
2025-12-24 16:18:41 +0100

    feat: add Story model in stories app

--- stories/models.py
+++ stories/models.py
@@ -1,2 +1,18 @@
 from django.db import models
+from accounts.models import Author
+
+class Story(models.Model):
+    author = models.ForeignKey(Author, on_delete=models.SET_NULL, null=True, related_name='stories')
+    title = models.CharField(250)
+    GENRE_CHOICES = (
+        ('mystery', 'Mystery'), 
+        ('fiction', 'Fiction'),
+        ('comedy', 'Comedy'),
+        ('others', 'Others'),
+    )
+    genre = models.CharField(max_length=7, choices=GENRE_CHOICES, default='others')
+    context = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    likes = models.PositiveIntegerField(default=0)
+    dislikes = models.PositiveIntegerField(default=0)
 
