fb3b28966f94c42db555372114d6e6f056c9d376
JhayceeCodes <139480167+JhayceeCodes@users.noreply.github.com>
2025-12-25 01:07:50 +0100

    feat: add Story and StoryReaction models

--- stories/models.py
+++ stories/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from accounts.models import Author
+from accounts.models import Author, User
 
 class Story(models.Model):
     author = models.ForeignKey(Author, on_delete=models.SET_NULL, null=True, related_name='stories')
@@ -10,9 +10,26 @@ class Story(models.Model):
         ('comedy', 'Comedy'),
         ('others', 'Others'),
     )
-    genre = models.CharField(max_length=7, choices=GENRE_CHOICES, default='others')
+    genre = models.CharField(max_length=7, choices=GENRE_CHOICES)
     context = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     likes = models.PositiveIntegerField(default=0)
     dislikes = models.PositiveIntegerField(default=0)
 
+class StoryReaction(models.Model):
+    REACTION_CHOICES = (
+        ('like', 'Like'), 
+        ('dislike', 'Dislike'),
+    )
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    story = models.ForeignKey(Story, on_delete=models.CASCADE, related_name="reactions")
+    reaction = models.CharField(max_length=7, choices=REACTION_CHOICES)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["user", "story"],
+                name="unique_user_story_reaction"
+            )
+        ]
