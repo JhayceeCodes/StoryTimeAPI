aebb513d8e2560512eda307c7a745cfc152319db
JhayceeCodes <139480167+JhayceeCodes@users.noreply.github.com>
2026-01-08 03:32:25 +0100

    feat: update cache backend to RedisCache

--- StoryTime/settings.py
+++ StoryTime/settings.py
@@ -158,10 +158,14 @@ SIMPLE_JWT = {
 
 CACHES = {
     "default": {
-        "BACKEND": "django.core.cache.backends.locmem.LocMemCache",
-        "LOCATION": "unique-storytime-cache",
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
     }
 }
+
 
 
 DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL")
