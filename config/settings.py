ab810408a8b4a43c85d86698eb14d032d17d43d2
JhayceeCodes <139480167+JhayceeCodes@users.noreply.github.com>
2026-01-22 06:02:03 +0100

    feat: rename project app name to config

--- config/settings.py
+++ config/settings.py
@@ -59,7 +59,7 @@ EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL', EMAIL_HOST_USER)
 
-ROOT_URLCONF = 'StoryTime.urls'
+ROOT_URLCONF = 'config.urls'
 
 TEMPLATES = [
     {
@@ -76,7 +76,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'StoryTime.wsgi.application'
+WSGI_APPLICATION = 'config.wsgi.application'
 
 
 # Database
